ffb3b346f7a54f91ae46df19b3814e87351e20d8
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-25 15:03:08 -0600

    finished chapter 5 practice

--- src/components/owner/OwnerList.js
+++ src/components/owner/OwnerList.js
@@ -12,16 +12,24 @@ const OwnerList = () => {
     });
   };
 
+  const deleteOwner = id => {
+    OwnerManager.delete(id)
+      .then(() => OwnerManager.getAll().then(setOwners));
+  };
+
   useEffect(() => {
     getOwners();
   }, []);
 
-  return(
+  return (
     <div className="container-cards">
       {owners.map(owner =>
-        <OwnerCard key={owner.id} owner={owner} />
-      )}
+        <OwnerCard
+          key={owner.id}
+          owner={owner}
+          deleteOwner={deleteOwner} />)}
     </div>
   );
 };
+
 export default OwnerList;
