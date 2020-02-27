cf4ce89c53fb324040020a38fcf393ee5c61f7f5
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-27 15:45:31 -0600

    finished first challenge exercise

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -4,44 +4,50 @@ import "./AnimalDetail.css";
 import { firstLetterCase } from '../../modules/helpers';
 
 const AnimalDetail = props => {
-  const [animal, setAnimal] = useState({ name: "", breed: "" });
-  const [isLoading, setIsLoading] = useState(true);
+    const [animal, setAnimal] = useState({ name: "", breed: "" });
+    const [isLoading, setIsLoading] = useState(true);
 
-  const handleDelete = () => {
-    //invoke the delete function in AnimalManger and re-direct to the animal list.
-    setIsLoading(true);
-    AnimalManager.delete(props.animalId).then(() =>
-      props.history.push("/animals")
-    );
-  };
+    const handleDelete = () => {
+        setIsLoading(true);
+        AnimalManager.delete(props.animalId).then(() =>
+            props.history.push("/animals")
+        );
+    };
 
-  useEffect(() => {
-    //get(id) from AnimalManager and hang on to the data; put it into state
-    AnimalManager.get(props.animalId).then(animal => {
-      setAnimal({
-        name: animal.name,
-        breed: animal.breed
-      });
-      setIsLoading(false);
-    });
-  }, [props.animalId]);
+    useEffect(() => {
+        AnimalManager.get(props.animalId).then(animal => {
+            setAnimal({
+                name: animal.name,
+                breed: animal.breed
+            });
+            setIsLoading(false);
+        });
+    }, [props.animalId]);
 
-  return (
-    <div className="card">
-      <div className="card-content">
-        <picture>
-          <img src={require("./dog.svg")} alt="My Dog" />
-        </picture>
-        <h3>
-          Name: <span style={{ color: "darkslategrey" }}>{firstLetterCase(animal.name)}</span>
-        </h3>
-        <p>Breed: {firstLetterCase(animal.breed)}</p>
-        <button type="button" disabled={isLoading} onClick={handleDelete}>
-          Discharge
-        </button>
-      </div>
-    </div>
-  );
+    if (animal.name === undefined) {
+        return (
+            <div className="notFound">
+                <h2>The animal you are searching for does not exist</h2>
+            </div>
+        )
+    } else {
+        return (
+            <div className="card">
+                <div className="card-content">
+                    <picture>
+                        <img src={require("./dog.svg")} alt="My Dog" />
+                    </picture>
+                    <h3>
+                        Name: <span style={{ color: "darkslategrey" }}>{firstLetterCase(animal.name)}</span>
+                    </h3>
+                    <p>Breed: {firstLetterCase(animal.breed)}</p>
+                    <button type="button" disabled={isLoading} onClick={handleDelete}>
+                        Discharge
+            </button>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default AnimalDetail;
