36cd39abb49fb9d56008b19cd82746315e24b141
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-27 14:11:27 -0600

    finished chapter 10 practice

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -8,11 +8,10 @@ import OwnerList from "./owner/OwnerList";
 import AnimalDetail from "./animal/AnimalDetail";
 import LocationDetail from "./location/LocationDetail";
 import AnimalForm from './animal/AnimalForm';
-import Login from "./auth/Login";
 import LocationForm from './location/LocationForm';
 import EmployeeForm from './employee/EmployeeForm';
 import OwnerForm from './owner/OwnerForm';
-
+import Login from "./auth/Login";
 
 const ApplicationViews = () => {
   const isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
@@ -39,63 +38,99 @@ const ApplicationViews = () => {
       <Route
         path="/animals/new"
         render={(props) => {
-          return <AnimalForm {...props} />
+          if (isAuthenticated()) {
+            return <AnimalForm {...props} />
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
         path="/animals/:animalId(\d+)"
         render={props => {
-          return (
-            <AnimalDetail
-              animalId={parseInt(props.match.params.animalId)}
-              {...props}
-            />
-          );
+          if (isAuthenticated()) {
+            return (
+              <AnimalDetail
+                animalId={parseInt(props.match.params.animalId)}
+                {...props}
+              />
+            );
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route exact
         path="/locations"
         render={(props) => {
-          return <LocationList {...props} />
+          if (isAuthenticated()) {
+            return <LocationList {...props} />
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
         path="/locations/new"
         render={(props) => {
-          return <LocationForm {...props} />
+          if (isAuthenticated()) {
+            return <LocationForm {...props} />
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
         path="/locations/:locationId(\d+)" render={(props) => {
-          return <LocationDetail locationId={parseInt(props.match.params.locationId)}
+          if (isAuthenticated()) {
+            return <LocationDetail locationId={parseInt(props.match.params.locationId)}
             {...props}
           />
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
         exact
         path="/employees"
         render={props => {
-          return <EmployeeList {...props} />;
+          if (isAuthenticated()) {
+            return <EmployeeList {...props} />;
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
         path="/employees/new"
         render={(props) => {
-          return <EmployeeForm {...props} />
+          if (isAuthenticated()) {
+            return <EmployeeForm {...props} />
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
         exact
         path="/owners"
         render={props => {
-          return <OwnerList {...props} />;
+          if (isAuthenticated()) {
+            return <OwnerList {...props} />;
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
         path="/owners/new"
         render={(props) => {
-          return <OwnerForm {...props} />
+          if (isAuthenticated()) {
+            return <OwnerForm {...props} />
+          } else {
+            return <Redirect to="/login" />
+          }
         }}
       />
       <Route
